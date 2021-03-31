6ead98d2e2319b0e24a756703e48f4846bf864d5
andrsn <andrsn@umich.edu>
2021-03-30 22:11:25 -0400

    fixing test cases

--- Project2.py
+++ Project2.py
@@ -214,13 +214,13 @@ class TestCases(unittest.TestCase):
         self.assertEqual(len(csv_lines), 21)
 
         # check that the header row is correct
-        self.assertEqual(csv_lines[0], "Book title,Author Name")
+        self.assertEqual(csv_lines[0], ["Book title","Author Name"])
 
         # check that the next row is 'Harry Potter and the Deathly Hallows (Harry Potter, #7)', 'J.K. Rowling'
-        self.assertEqual(csv_lines[1], 'Harry Potter and the Deathly Hallows (Harry Potter, #7)', 'J.K. Rowling')
+        self.assertEqual(csv_lines[1], ['Harry Potter and the Deathly Hallows (Harry Potter, #7)', 'J.K. Rowling'])
 
         # check that the last row is 'Harry Potter: The Prequel (Harry Potter, #0.5)', 'J.K. Rowling'
-        self.assertEqual(csv_lines[len(csv_lines)-1], 'Harry Potter: The Prequel (Harry Potter, #0.5)', 'J.K. Rowling')
+        self.assertEqual(csv_lines[len(csv_lines)-1], ['Harry Potter: The Prequel (Harry Potter, #0.5)', 'J.K. Rowling'])
 
 
 
